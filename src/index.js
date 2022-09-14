d5397e632dd626ae3e95fc8d0ce73c20debf175a
RITIK RANAUT <rranaut76@gmail.com>
2022-09-14 23:38:03 +0530

    First Commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SortVisual from './SortVisual';
+
+
+ReactDOM.render(
+    <>
+        <SortVisual />
+    </>
+    ,document.getElementById("root")
+);
